983d48737cc1050abab338b6052d7a28f813788c
Sreenath285 <sreenath.285@gmail.com>
2022-04-07 22:25:18 +0530

    Storing daily wage in an array

---
+++ EmployeeWageJS.js
@@ -0,0 +1,39 @@
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUMBER_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArray = new Array();
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
+      totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArray.push(calculateDailyWage(empHrs));
+}
+
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total days worked : " + totalWorkingDays +
+                "\nTotal hours worked : " + totalEmpHrs + "\nEmployee wage: " + empWage);
+
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+function calculateDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
